c6bb69cc9fe03ffaf8c090d1e325efca96fca02b
Scott Vossen <scottvossen@gmail.com>
2014-01-08 15:52:18 -0600

    clean up excessive commenting

---
+++ js/view/todos-view.js
@@ -0,0 +1,38 @@
+var TodosView = Backbone.View.extend({
+  
+  tagName: 'ul',
+  className: 'container',
+  id: 'todos',
+
+  template: TemplateHelper.loadTemplate('todos-template'),
+
+  events: {
+    'dblclick label': 'edit',
+    'keypress .edit': 'updateOnEnter',
+    'blur .edit':   'close'
+  },
+
+  initialize: function() {
+  },
+
+  render: function() {
+
+    // Assume our model exposes the todos we will
+    // display in our list
+    var todos = this.model.get('todos');
+
+    // Loop through each of our todos using the Underscore
+    // _.each iterator
+    _.each(todos, function(todo){
+
+      // Create a new instance of the ItemView, passing 
+      // it a specific model todo
+      var todoView = new TodoView({ model: todo });
+      // The todoView's DOM element is appended after it
+      // has been rendered. Here, the 'return this' is helpful
+      // as the todoView renders its model. Later, we ask for 
+      // its output ("el")
+      this.$el.append(todoView.render().el);
+    }, this);
+  },
+});
