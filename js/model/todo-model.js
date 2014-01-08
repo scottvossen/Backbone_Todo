7fc7904732f1e355884d7a32e22967584bef2d7c
Scott Vossen <scottvossen@gmail.com>
2014-01-08 15:20:46 -0600

    add some validation

--- js/model/todo-model.js
+++ js/model/todo-model.js
@@ -5,7 +5,15 @@ var Todo = Backbone.Model.extend({
     completed: false
   },
 
+  validate: function(attributes){
+    if (attributes.title === undefined) {
+        return "Remember to set a title for your todo.";
+    }
+  },
+
   initialize: function() {
-    console.log('This model has been initialized.');
+    this.on("invalid", function(model, error){
+        console.log(error);
+    });
   }
 });
