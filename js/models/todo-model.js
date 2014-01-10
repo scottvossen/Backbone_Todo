9147ec7bd9553fcd754166d7349a277c1511df76
Scott Vossen <scottvossen@gmail.com>
2014-01-10 12:26:05 -0600

    update todo model

--- js/models/todo-model.js
+++ js/models/todo-model.js
@@ -1,19 +1,18 @@
-var Todo = Backbone.Model.extend({
+var app = app || {};
 
+app.Todo = Backbone.Model.extend({
+
+  // Default attributes ensure that each todo created has `title` and `completed` keys.
   defaults: {
     title: '',
     completed: false
   },
 
-  validate: function(attributes) {
-    if (attributes.title === undefined) {
-        return "Remember to set a title for your todo.";
-    }
-  },
-
-  initialize: function() {
-    this.on("invalid", function(model, error){
-        console.log(error);
+  // Toggle the `completed` state of this todo item.
+  toggle: function() {
+    this.save({
+      completed: !this.get('completed')
     });
-  },
+  }
+
 });
