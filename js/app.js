bf2a82169ad2d524737b4b4fb99bf832052ab1bf
Scott Vossen <scottvossen@gmail.com>
2014-01-10 13:03:32 -0600

    update app to kick things off

--- js/app.js
+++ js/app.js
@@ -1,18 +1,28 @@
-$(window).bind("load", function() {
+// $(window).bind("load", function() {
 
-	var todo1 = new Todo({
-	  title: 'Relax on couch.'
-	});
-	var todo2 = new Todo({
-	  title: 'Go to mall.'
-	});
+// 	var todo1 = new Todo({
+// 	  title: 'Relax on couch.'
+// 	});
+// 	var todo2 = new Todo({
+// 	  title: 'Go to mall.'
+// 	});
 
-	var todoCollection = new TodoCollection([todo1, todo2]);
+// 	var todoCollection = new TodoCollection([todo1, todo2]);
 
-	var todosContainer = $('#todos');
-	var todosView = new TodosView({ model: todoCollection, container: todosContainer });
+// 	var todosContainer = $('#todos');
+// 	var todosView = new TodosView({ model: todoCollection, container: todosContainer });
 
-	todosView.render();
+// 	todosView.render();
 
-	console.log(todosView.el);
+// 	console.log(todosView.el);
+// });
+
+var app = app || {};
+var ENTER_KEY = 13;
+
+$(function() {
+
+// Kick things off by creating the **App**.
+new app.AppView();
+
 });
