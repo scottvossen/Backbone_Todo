9348b0abb07df4205fd4f1fb43bb1d3a884a2c0a
Scott Vossen <scottvossen@gmail.com>
2014-01-10 12:42:32 -0600

    add comments

--- js/models/todo-model.js
+++ js/models/todo-model.js
@@ -1,5 +1,8 @@
 var app = app || {};
 
+// Todo Model
+// ----------
+// Our basic **Todo** model has `title`, `order`, and `completed` attributes.
 app.Todo = Backbone.Model.extend({
 
   // Default attributes ensure that each todo created has `title` and `completed` keys.
