a661e4bbb5d48720da9b5a5d20db304a20a5e124
Scott Vossen <scottvossen@gmail.com>
2014-01-10 10:02:58 -0600

    bind app stuff to window load event

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
-var todo1 = new Todo({
-  title: 'Relax on couch.'
+$(window).bind("load", function() {
+
+	var todo1 = new Todo({
+	  title: 'Relax on couch.'
+	});
+	var todo2 = new Todo({
+	  title: 'Go to mall.'
+	});
+
+	var todoCollection = new TodoCollection([todo1, todo2]);
+
+	var todosContainer = $('#todos');
+	var todosView = new TodosView({ model: todoCollection, container: todosContainer });
+
+	todosView.render();
+
+	console.log(todosView.el);
 });
-var todo2 = new Todo({
-  title: 'Go to mall.'
-});
-
-var todoCollection = new TodoCollection([todo1, todo2]);
-
-var todosContainer = $('#todos');
-var todosView = new TodosView({ model: todoCollection, container: todosContainer });
-
-todosView.render();
-
-console.log(todosView.el);
